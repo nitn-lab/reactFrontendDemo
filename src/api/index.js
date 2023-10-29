57c64d49ee26362f6e54d7af02ffbbe86d513cdc
M7839 <dolly.rajput.hq@hitachi-systems.com>
2023-10-29 18:02:40 +0530

    url updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3000" });
+const API = axios.create({ baseURL: "https://reactbackend-demo.onrender.com" });
 // const url = 'https://reactbackend-demo.onrender.com/posts';
 // const url = 'http://localhost:3000/posts';
 
